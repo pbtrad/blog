5e8e54134c4bcab6ae0c21e80d88d3d5094693bd
Paul Browne <paulbrowne2@gmail.com>
2021-08-24 09:45:29 +0100

    Blog detail template created, form for comments created in forms.py, and Comment model created

--- blog/blog_main/views.py
+++ blog/blog_main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .forms import CommentForm
+from .models import Post
 
-# Create your views here.
+def detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    form = CommentForm()
+
+    return render(request, 'blog_main/detail.html', {'post': post, 'form': form})
